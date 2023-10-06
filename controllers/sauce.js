31a3174a50b2a8953f794a76d74abd3dccf1678c
Julien Jean <julienjean9@gmail.com>
2023-10-06 16:17:41 +0200

    Création des différentes routes

---
+++ controllers/sauce.js
@@ -0,0 +1,88 @@
+const Sauce = require('../models/sauce');
+const fs = require('fs');
+
+exports.createSauce = (req, res, next) => {
+    const sauceObject = JSON.parse(req.body.Sauce);
+    delete sauceObject._id;
+    delete sauceObject._userId;
+    const sauce = new Sauce({
+        ...SauceObject,
+        userId: req.auth.userId,
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    });
+
+    sauce.save()
+        .then(() => { res.status(201).json({ message: 'Sauce enregistré !' }) })
+        .catch(error => { res.status(400).json({ error }) })
+};
+
+exports.getOneSauce = (req, res, next) => {
+    Sauce.findOne({
+        _id: req.params.id
+    }).then(
+        (Sauce) => {
+            res.status(200).json(Sauce);
+        }
+    ).catch(
+        (error) => {
+            res.status(404).json({
+                error: error
+            });
+        }
+    );
+};
+
+exports.modifySauce = (req, res, next) => {
+    const SauceObject = req.file ? {
+        ...JSON.parse(req.body.Sauce),
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : { ...req.body };
+
+    delete SauceObject._userId;
+    Sauce.findOne({ _id: req.params.id })
+        .then((Sauce) => {
+            if (Sauce.userId != req.auth.userId) {
+                res.status(401).json({ message: 'Not authorized' });
+            } else {
+                Sauce.updateOne({ _id: req.params.id }, { ...SauceObject, _id: req.params.id })
+                    .then(() => res.status(200).json({ message: 'Sauce modifiée !' }))
+                    .catch(error => res.status(401).json({ error }));
+            }
+        })
+        .catch((error) => {
+            res.status(400).json({ error });
+        });
+};
+
+exports.deleteSauce = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+        .then(Sauce => {
+            if (Sauce.userId != req.auth.userId) {
+                res.status(401).json({ message: 'Not authorized' });
+            } else {
+                const filename = Sauce.imageUrl.split('/images/')[1];
+                fs.unlink(`images/${filename}`, () => {
+                    Sauce.deleteOne({ _id: req.params.id })
+                        .then(() => { res.status(200).json({ message: 'Sauce supprimée !' }) })
+                        .catch(error => res.status(401).json({ error }));
+                });
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ error });
+        });
+};
+
+exports.getAllSauces = (req, res, next) => {
+    Sauce.find().then(
+        (Sauces) => {
+            res.status(200).json(Sauces);
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                error: error
+            });
+        }
+    );
+};
