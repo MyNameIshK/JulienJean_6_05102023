d7bb00cedf7c9716997984c98112ae3afaad29ac
Julien Jean <julienjean9@gmail.com>
2023-10-11 19:43:44 +0200

    Test en cas de deux userID

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,10 +5,11 @@ module.exports = (req, res, next) => {
         const token = req.headers.authorization.split(' ')[1];
         const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
         const userId = decodedToken.userId;
-        req.auth = {
-            userId: userId
+        if (req.body.userId && req.body.userId !== userId) {
+            throw "Utilisateur non valide.";
+          } else {
+            next();
         };
-        next();
     } catch (error) {
         res.status(401).json({ error });
     }
