d8a413e4ec1747385ddd077af3975a905737b5bb
Julien Jean <julienjean9@gmail.com>
2023-10-20 17:53:28 +0200

    Modification du fichier password.js

--- models/password.js
+++ models/password.js
@@ -3,20 +3,12 @@ const passwordValidator = require('password-validator');
 const passwordSchema = new passwordValidator();
 
 passwordSchema
-  .is()
-  .min(8) // Longueur minimum de 8 lettres.
-  .is()
-  .max(20) // Longueur maximale de 20 lettres.
-  .has()
-  .uppercase(1) // Au moins 1 lettre majuscule.
-  .has()
-  .lowercase(1) // Au moins 1 lettre minuscule.
-  .has()
-  .digits(2) // Au moins 2 chiffres.
-  .has()
-  .symbols(1) // Au moins 1 caractère spécial.
-  .has()
-  .not()
-  .spaces() // Ne doit pas contenir d'espaces.
+  .is().min(8) // Longueur minimum de 8 lettres.
+  .is().max(20) // Longueur maximale de 20 lettres.
+  .has().uppercase(1) // Au moins 1 lettre majuscule.
+  .has().lowercase(1) // Au moins 1 lettre minuscule.
+  .has().digits(2) // Au moins 2 chiffres.
+  .has().symbols(1) // Au moins 1 caractère spécial.
+  .has().not().spaces() // Ne doit pas contenir d'espaces.
 
 module.exports = passwordSchema;
